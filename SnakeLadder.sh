0bc4925838a9293ad3eda814d7f2207c95933ea8
mvinchankar <mvinchankar18@gmail.com>
2019-11-14 16:38:20 +0530

    ADDED Function Plays to play and move the player using cases

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -3,5 +3,20 @@ echo ---------------------Welcome To Snake and Ladder World--------------------
 
 START=0;
 PLAYER=1;
-random=$((RANDOM%6+1))
-echo You Got :$random
+NO=0;
+LADDER=1;
+SNAKE=2;
+position=0;
+random=$((RANDOM%3))
+random1=$((RANDOM%6+1))
+function Plays()
+{
+ case $random in $NO)
+      position=0;;
+                $LADDER)
+      positon=$(($postion+$random1));;
+                $Snake)
+      positon=$(($postion-$random1));;
+ esac  
+}
+Plays
