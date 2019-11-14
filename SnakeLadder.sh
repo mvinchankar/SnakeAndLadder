b53f6ffb005c4f2933a9d65d48db43af4d39e0d0
mvinchankar <mvinchankar18@gmail.com>
2019-11-14 17:35:44 +0530

    ADDED Count the number of times dice was played to win the game and also Position

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -7,21 +7,25 @@ NO=0;
 LADDER=2;
 SNAKE=1;
 position=0;
+diceCount=0;
 function Plays()
 { 
   while [ $position -le 100  ]
   do
    random=$((RANDOM%3))
    random1=$((RANDOM%6+1))
+   diceCount=$((diceCount+1)) 
    case $random in  $SNAKE )
         position=$(($position-$random1));;
                    $LADDER )
         position=$(($position+$random1));;
    esac
+   
    if [ $position -le 0 ]
    then
        position=0;
    fi
+   echo Postion : $position
    if [ $position -gt 100 ]
    then 
        position=$(($position-$random1))
@@ -34,3 +38,4 @@ function Plays()
   done
 } 
 Plays
+echo Dice Count : $diceCount
