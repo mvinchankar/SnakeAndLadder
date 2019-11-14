b790518af19dd3585b1ffc7f590c13ea8a4b09bb
mvinchankar <mvinchankar18@gmail.com>
2019-11-14 19:30:34 +0530

    ADDED Condition to play the game with Two Players

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -2,19 +2,25 @@
 echo ---------------------Welcome To Snake and Ladder World--------------------
 
 START=0;
-PLAYER=1;
 NO=0;
 LADDER=2;
 SNAKE=1;
 position=0;
 diceCount=0;
-function Plays()
+playerOneCount=0;
+playerTwoCount=0;
+count1=0;
+count2=0;
+playerOnePosition=0;
+playerTwoPosition=0;
+function plays()
 { 
+  
   while [ $position -le 100  ]
   do
    random=$((RANDOM%3))
    random1=$((RANDOM%6+1))
-   diceCount=$((diceCount+1)) 
+   diceCount=$(($diceCount+1))
    case $random in  $SNAKE )
         position=$(($position-$random1));;
                    $LADDER )
@@ -25,17 +31,45 @@ function Plays()
    then
        position=0;
    fi
-   echo Postion : $position
    if [ $position -gt 100 ]
    then 
        position=$(($position-$random1))
    fi
-   if [ $position -eq 100 ]
-   then 
-       echo You Win
+   if [ $playerOnePosition -eq 100 ]
+   then  
+       win=1 
+       echo PlayerOne Win
        break;  
    fi
+   if [ $playerTwoPosition -eq 100 ]
+   then 
+       win=1
+       echo Player Two Win
+       break;
+   fi
+   if [ $playerOneCount == 1 ]
+   then
+      playerOneCount=0;
+   elif [ $playerOneCount == 0 ]
+   then 
+      playerOnePosition=$(($position))
+      playerOneCount=1;
+      echo PlayerOnePosition : $position
+   fi
+   if [ $playerOneCount == 1 ]
+   then 
+      playerOneCount=2;
+      if [ playerOneCount == 2 ]
+      then
+          playerTwoCount=0; 
+          if [ $playerTwoCount == 0 ]
+          then
+             playerTwoCount=$(($playerTwoCount+1))
+             playerTwoPosition=$(($position))
+             echo PlayerTwoPosition : $position
+          fi
+      fi
+   fi
   done
-} 
-Plays
-echo Dice Count : $diceCount
+}
+plays
